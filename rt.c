05d4b973cd56e313515f694df81154bba85264b4
Rahul Bhushan <rahulbhushan@rahuls-air.speedport_w_723v_1_37_000>
2015-08-31 23:38:11 +0200

    Revert "rt.c modified from commit address faed052"
    This reverts commit 52fb397af3c979bae57f150ac00a2cc8b336a0f7.

--- rt.c
+++ rt.c
@@ -1,35 +1,16 @@
 #include <stdio.h>
 
 
-int add(int a, int b);
-
-
-int add(int a,int b)
-{
- int c;
- c=a+b;
-
-return c;
-
-}
-
-
 int main()
 {
-  int a1,b1;
+ 
+  int a,b;
   
-// this is a wrong variable that was wrongly added in commit 17d1e4f
-  //int bh;
+int cd;
+ind cd =a+b;
 
 
-
-
-printf("hello ui");
-
-
-//function call here
-printf("result of addition is:%d", add(3,2));
-
+  printf("hello ui");
 return (0);
 }
 
